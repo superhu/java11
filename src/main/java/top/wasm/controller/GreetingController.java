0031ff5c55448323913f10430678d8f6a1547ce5
huzhichao <569791786@qq.com>
2018-09-30 09:51:41 +0800

    add profile and map static images

--- src/main/java/top/wasm/controller/GreetingController.java
+++ src/main/java/top/wasm/controller/GreetingController.java
@@ -5,15 +5,15 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
+
 @Controller
 public class GreetingController {
-
 
 
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
-        Thread.sleep(1000); // simulated delay
+        System.out.println(message.getName());
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
 
--- src/main/java/top/wasm/controller/HelloController.java
+++ src/main/java/top/wasm/controller/HelloController.java
@@ -1,22 +1,29 @@
-package top.wasm.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/")
-public class HelloController {
-
-    @GetMapping("/hi")
-    public String hello(@ModelAttribute("name") String name){
-
-
-
-        return "hello "+name;
-    }
-
-
-
-}
+//package top.wasm.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/")
+//public class HelloController {
+//
+//    @GetMapping("/hi")
+//    public String hello(@ModelAttribute("name") String name){
+//
+//
+//
+//        return "hello "+name;
+//    }
+//    @GetMapping("/")
+//    public String index(){
+//
+//
+//
+//        return "index";
+//    }
+//
+//
+//
+//}
