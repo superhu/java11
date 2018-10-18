6bbe1e854799cebd9fc34380ce8f8fd186949c6e
huzhichao <569791786@qq.com>
2018-10-18 17:58:10 +0800

    https test

--- src/main/java/top/wasm/controller/GreetingController.java
+++ src/main/java/top/wasm/controller/GreetingController.java
@@ -1,20 +1,17 @@
 package top.wasm.controller;
 
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.util.HtmlUtils;
 
 
 @Controller
 public class GreetingController {
 
 
-    @MessageMapping("/hello")
-    @SendTo("/topic/greetings")
-    public Greeting greeting(HelloMessage message) throws Exception {
-        System.out.println(message.getName());
-        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
-    }
+//    @MessageMapping("/hello")
+//    @SendTo("/topic/greetings")
+//    public Greeting greeting(HelloMessage message) throws Exception {
+//        System.out.println(message.getName());
+//        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+//    }
 
 }
--- src/main/java/top/wasm/controller/HelloController.java
+++ src/main/java/top/wasm/controller/HelloController.java
@@ -1,5 +1,6 @@
 package top.wasm.controller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,40 +9,50 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/index")
+@RequestMapping("/")
 public class HelloController {
     @Value("static_images_path")
     private String static_images_path;
 
-    @GetMapping("/images")
+//    @GetMapping("/images")
+//    @ResponseBody
+//    public String[] images(){
+//
+//
+//        File filePath = new File(static_images_path);
+//        String[] fileList = filePath.list(new FilenameFilter() {
+//            @Override
+//            public boolean accept(File dir, String name) {
+//                return name.toLowerCase().endsWith(".jpg") || name.toLowerCase().endsWith(".jpeg") || name.toLowerCase().endsWith(".png");
+//            }
+//        });
+//
+//        return fileList;
+//    }
+//    @GetMapping("/mp3")
+//    public String[] mp4(){
+//
+//        File filePath = new File(static_images_path);
+//        String[] fileList = filePath.list(new FilenameFilter() {
+//            @Override
+//            public boolean accept(File dir, String name) {
+//                return name.toLowerCase().endsWith(".mp4");
+//            }
+//        });
+//
+//        return fileList;
+//    }
+
+    @GetMapping("/hello")
     @ResponseBody
-    public String[] images(){
-
-
-        File filePath = new File(static_images_path);
-        String[] fileList = filePath.list(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.toLowerCase().endsWith(".jpg") || name.toLowerCase().endsWith(".jpeg") || name.toLowerCase().endsWith(".png");
-            }
-        });
-
-        return fileList;
-    }
-    @GetMapping("/mp3")
-    public String[] mp4(){
-
-        File filePath = new File(static_images_path);
-        String[] fileList = filePath.list(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.toLowerCase().endsWith(".mp4");
-            }
-        });
-
-        return fileList;
+    public Map test(){
+        Map m = new HashMap();
+        m.put("result","ok");
+        return m;
     }
 
 
